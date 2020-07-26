e211dbea76c0af969fcc726825ea4329455099f4
shahab7758 <shahabm298@gmail.com>
2020-07-26 19:31:54 +0500

    Added Grid

---
+++ src/Grid.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Grid extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="a">A</div>
+        <div className="b">B</div>
+        <div className="c">C</div>
+        <div className="d">D</div>
+        <div className="e">E</div>
+        <div className="f">F</div>
+        <div className="g">G</div>
+        <div className="h">H</div>
+      </div>
+    );
+  }
+}
